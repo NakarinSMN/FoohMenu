22f40642b84496e57c74dd20d7a9238b5df2f3bd
SMN Code <102906136+NakarinSMN@users.noreply.github.com>
2025-07-16 13:51:52 +0700

    ๊Update

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="th">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body>
         <div className="header">
           <div className="head-main">
